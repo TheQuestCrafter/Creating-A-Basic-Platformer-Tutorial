0731d46009ccd8eb5e54d0c3c6c74888c788a0be
TheQuestCrafter <thequestcrafter@gmail.com>
2018-10-16 03:19:34 -0500

    Platform Spawning

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    public int maxPlatforms = 20;
+    public GameObject platform;
+    public float horizontalMin = 6.5f, horizontalMax = 14f, verticalMin = -6f, verticalMax = 6f;
+
+    private Vector2 originPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        originPosition = transform.position;
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        for (int i = 0; i < maxPlatforms; i++)
+        {
+            Vector2 randomPosition = originPosition + new Vector2(Random.Range(horizontalMin, horizontalMax), Random.Range(verticalMin, verticalMax));
+            Instantiate(platform, randomPosition, Quaternion.identity);
+            originPosition = randomPosition;
+        }
+    }
+}
